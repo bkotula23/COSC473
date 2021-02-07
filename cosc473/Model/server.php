f4ef21c5883e11cafa819899c58ec2b97e9a5810
cloudlightfellow <72762479+cloudlightfellow@users.noreply.github.com>
2021-02-07 18:18:42 -0500

    Update server.php

--- cosc473/Model/server.php
+++ cosc473/Model/server.php
@@ -4,8 +4,7 @@ session_start();
 // initializing variables
 $username = "";
 $email    = "";
-$errors = array(); 
-
+ 
 // connect to the database
 $db = mysqli_connect('localhost', 'root', '', 'cosc473');
 
@@ -19,11 +18,11 @@ if (isset($_POST['reg_user'])) {
 
   // form validation: ensure that the form is correctly filled ...
   // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { array_push($errors, "Username is required"); }
-  if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if (empty($username)) { echo ("Username is required, ");} 
+  if (empty($email)) { echo ("Email is required, "); }
+  if (empty($password_1)) { echo ( "Password is required, "); }
   if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
+	echo ("The two passwords do not match, ");
   }
 
   // first check the database to make sure 
@@ -32,26 +31,32 @@ if (isset($_POST['reg_user'])) {
   $result = mysqli_query($db, $user_check_query);
   $user = mysqli_fetch_assoc($result);
   
-  if ($user) { // if user exists
+	if ($user) { // if user exists
     if ($user['username'] === $username) {
-      array_push($errors, "Username already exists");
+      echo ("Username already exists");
     }
 
-    if ($user['email'] === $email) {
-      array_push($errors, "email already exists");
+    elseif ($user['email'] === $email) {
+      echo ( "email already exists");
     }
   }
 
   // Finally, register user if there are no errors in the form
-  if (count($errors) == 0) {
+  else {
   	$password = md5($password_1);//encrypt the password before saving in the database
 
   	$query = "INSERT INTO users (username, email, password) 
   			  VALUES('$username', '$email', '$password')";
-  	mysqli_query($db, $query);
+  	$success = mysqli_query($db, $query);
   	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+	if ($success) {
+		echo ("Registration Successful!");
+	}
+	else{
+		echo ("Failed to Register");
+	}
+  	//$_SESSION['success'] = "You are now logged in";
+  	include ("view/login.php");
   }
 }
 ?> 
