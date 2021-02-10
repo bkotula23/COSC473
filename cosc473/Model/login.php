f2280401f5556ae62b7447c8db18d3d02e9448ca
cloudlightfellow <72762479+cloudlightfellow@users.noreply.github.com>
2021-02-10 10:48:58 -0500

    Update login.php

--- cosc473/Model/login.php
+++ cosc473/Model/login.php
@@ -1,25 +1,25 @@
 <?php
 // LOGIN USER
+$db = mysqli_connect('localhost', 'root', '', 'cosc473');
 if (isset($_POST['login_user'])) {
   $username = mysqli_real_escape_string($db, $_POST['username']);
   $password = mysqli_real_escape_string($db, $_POST['password']);
 
   if (empty($username) || empty($password)) {
-  	echo("Username is required and Password is required");
-
+  	echo("Username and Password is required");
+	include "View/login.php";
+  }
   else {
   	$password = md5($password);
   	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
   	$results = mysqli_query($db, $query);
   	if (mysqli_num_rows($results) == 1) {
   	  $_SESSION['username'] = $username;
-  	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: index.php');
+	  echo ("Welcome $username!");
   	}else {
   		echo ("Wrong username/password combination");
+		include "View/login.php";
   	}
   }
 }
-
 ?> 
-<?
