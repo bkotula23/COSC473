af1ad5b9ffdca03bdcb08f89d324b75e09a42079
Seth Edwards <72762479+cloudlightfellow@users.noreply.github.com>
2021-02-24 09:44:47 -0500

    Add files via upload

---
+++ cosc473/ControllerStudent/cntrlStudent.php
@@ -0,0 +1,36 @@
+<?php
+
+include "ViewStudent/viewSitehome.php";
+
+class cntrCal {
+
+    public $ModelStudent, $ViewStudent;
+	
+	function __construct() {	
+	}
+	
+	function viewMenu(){		
+		
+		$this->view = new siteMenu();
+	}	
+	
+	function actions($flag) {	
+	
+	    $this->viewMenu();		
+		
+		if($flag=="viewCourses"){
+		   include "ViewStudent/viewCourses.php";
+		} 
+		if($flag=="viewCalandar"){
+		include "ViewStudent/viewCalandar.php";
+		} 
+		if($flag=="logout"){
+		   include "ModelStudent/logout.php";
+		}
+		if($flag=="verifyRegisterCourse"){
+		   include "ModelStudent/verifyCourseregistration.php";
+		}		
+		
+	}
+}
+?>
