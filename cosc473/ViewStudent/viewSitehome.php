2b7943d3eaf6354e06a8694ee712671a79e2c7d8
Seth Edwards <72762479+cloudlightfellow@users.noreply.github.com>
2021-03-08 15:14:47 -0500

    Update viewSitehome.php

--- cosc473/ViewStudent/viewSitehome.php
+++ cosc473/ViewStudent/viewSitehome.php
@@ -8,23 +8,23 @@
 ?>
 			<html>  
 			<head>  
-				<link rel="stylesheet" type="text/css" href="ViewStudent/style.css">
-				<title>Tutor IUP</title> 
-				
+				<link rel="stylesheet" type="text/css" href="view/style.css">
+				<title>Tutor IUP</title>  
 			</head> 
 
 				
 				<body>  
 							<h1>Tutor IUP - Student Dashboard</h1>	  
 							
-							<p> 		
-								<a href="indexStudent.php?flag=registerCourse"> View Courses</a>
+							<p id=links> 		
+								<a href="indexStudent.php?flag=viewCourses"> View Courses</a>
 								&nbsp;&nbsp;&nbsp;&nbsp;
 							    <a href="indexStudent.php"> Home</a>
 								&nbsp;&nbsp;&nbsp;&nbsp;
 								<a href="indexStudent.php?flag=changePassword"> Change Password</a>
 								&nbsp;&nbsp;&nbsp;&nbsp;
-							    <a href="indexStudent.php?flag=logout"> Logout</a>								
+							    <a href="indexStudent.php?flag=logout"> Logout</a>
+						<!--	<a href="indexStudent.php?flag=testUsername"> Check user</a>	username test session-->						
 							</p>							
 				</body>  
 
